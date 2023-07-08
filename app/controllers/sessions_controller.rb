ccecdb61cfd8e81c0bb6a8d93dbb2216e4b41daf
Di Lu <brotherludi@hotmail.com>
2023-07-07 20:42:20 -0600

    Fix Login and Navigation Bar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,16 @@
 class SessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
     @user = User.find_by(email: params[:email])
-    if @user && @user.authenticate(params[:password])
+
+    if @user&.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to root_path, notice: 'Logged in successfully!'
     else
-      flash.now[:alert] = 'Invalid email or password'
-      render :new
+      session[:user_id] = nil
+      flash[:alert] = 'Invalid email or password'
+      redirect_to login_path
     end
   end
 
